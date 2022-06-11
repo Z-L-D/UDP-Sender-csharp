d6fff38b6cb12d883c22e0140020b07adea9c258
unknown <zacldoyle@gmail.com>
2022-06-11 17:44:33 -0500

    Update all files

--- Program.cs
+++ Program.cs
@@ -9,27 +9,25 @@ namespace Project
     {
         public static void udp_client()
         {
-            Console.Write("UDP Message (Default: Hello): ");
+            Console.Write("UDP Message: ");
+
             string input = Console.ReadLine();
-            if(string.IsNullOrEmpty(input)){
-                input = "Hello";
-            }
-
-            byte[] input_encoded = new byte[1024];
-
             string ip_address = "127.0.0.1";
             int ip_port = 5500;
-            
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ip_address), ip_port);
-            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
 
             Console.WriteLine("Sending command: " + input);
             Console.WriteLine("To: " + ip_address + ":" + ip_port.ToString());
-
+            
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ip_address), ip_port);
+            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
+            
+            byte[] input_encoded = new byte[1024];
             input_encoded = Encoding.ASCII.GetBytes(input);
             server.SendTo(input_encoded, input_encoded.Length, SocketFlags.None, ip);
-
             server.Close();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
 
         static void Main(string[] args){
