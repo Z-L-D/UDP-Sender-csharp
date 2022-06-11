4c7b72e12b0a9c7651f0e651f20416e4bf51d025
unknown <zacldoyle@gmail.com>
2022-06-11 17:00:36 -0500

    Update all files

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Project
 {
     class program
     {
+        public static void udp_client(string var_a)
+        {
+            byte[] input_enc = new byte[1024];
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55398);
+            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
+
+            string input = var_a;
+
+            input_enc = Encoding.ASCII.GetBytes(input);
+            server.SendTo(input_enc, input_enc.Length, SocketFlags.None, ip);
+            Console.WriteLine("Sending command: " + var_a);
+
+            server.Close();
+        }
+
         static void Main(string[] args){
-            System.Console.WriteLine("Hello World");
+            udp_client("Eat Shit");
         }
     }
 }
