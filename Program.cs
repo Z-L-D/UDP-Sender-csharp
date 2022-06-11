44625ad504779e0483dc311fa5015d5aa4f7a83d
unknown <zacldoyle@gmail.com>
2022-06-11 17:07:15 -0500

    Update all files

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace Project
         public static void udp_client(string var_a)
         {
             byte[] input_enc = new byte[1024];
+            string ip_address = "127.0.0.1";
+            int ip_port = 55398;
+            string ip_port_string = ip_port.ToString();
 
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55398);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ip_address), ip_port);
             Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
 
             string input = var_a;
@@ -19,6 +22,7 @@ namespace Project
             input_enc = Encoding.ASCII.GetBytes(input);
             server.SendTo(input_enc, input_enc.Length, SocketFlags.None, ip);
             Console.WriteLine("Sending command: " + var_a);
+            Console.WriteLine("To: " + ip_address + ":" + ip_port_string);
 
             server.Close();
         }
