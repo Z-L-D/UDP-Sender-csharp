bcdd6f0dfccf1f01af39230cb9759ea84fedbd27
Z-L-D <zacldoyle@gmail.com>
2022-06-13 17:17:20 -0500

    -

--- UDP-Sender-csharp/Program.cs
+++ UDP-Sender-csharp/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
-namespace UDP_Sender_csharp
+namespace UDP_Sender
 {
-    internal class Program
+    class program
     {
+        static void udp_client()
+        {
+            Console.Write("UDP Command: ");
+
+            string input = Console.ReadLine();
+            string ip_address = "127.0.0.1";
+            int ip_port = 5500;
+
+            Console.WriteLine("Sending command: " + input);
+            Console.WriteLine("To: " + ip_address + ":" + ip_port.ToString());
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ip_address), ip_port);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            byte[] input_encoded = new byte[1024];
+            input_encoded = Encoding.ASCII.GetBytes(input);
+            server.SendTo(input_encoded, input_encoded.Length, SocketFlags.None, ip);
+            server.Close();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            udp_client();
         }
     }
 }
