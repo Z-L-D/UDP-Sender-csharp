862d9cb3883dacaf2bad087ca2d3addfc471b0e1
Z-L-D <zacldoyle@gmail.com>
2022-06-13 18:40:44 -0500

    -

--- UDP-Sender-csharp/Program.cs
+++ UDP-Sender-csharp/Program.cs
@@ -12,11 +12,11 @@ class program
         int port = 5500;
 
         IPEndPoint ip_object = new IPEndPoint(IPAddress.Parse(address), port);
-        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        Socket socket_object = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
         byte[] command_encoded = new byte[1024];
         command_encoded = Encoding.ASCII.GetBytes(command);
-        server.SendTo(command_encoded, command_encoded.Length, SocketFlags.None, ip_object);
-        server.Close();
+        socket_object.SendTo(command_encoded, command_encoded.Length, SocketFlags.None, ip_object);
+        socket_object.Close();
     }
 }
